732fe063a2f5e0b9c49797f538d4e4364af3dc7a
JoeC73 <josephcornell40@gmail.com>
2023-09-28 15:34:25 -0400

    Code and CSS  have been added to complete Lab exercise

--- dice.js
+++ dice.js
@@ -1,26 +1,27 @@
-function diceRoll(num, num2, Name1) {
+function diceRoll(num,num2,Name1) {
     console.log("this is how many side:", num, num2);
-    document.getElementById("roll6").innerHTML=numberMinMax(1,7)
+    document.getElementById(Name1).innerHTML = numberMinMax(num, num2);
 }
 
 
 
 
-function dice() {
-    console.log("button clicked")
-    console.log("this is the Dice1 dice")
-    numberMinMax(1, 6)
+function Dice6() {
+    console.log("button clicked");
+    console.log("This is the Dice6 dice");
+    numberMinMax(1, 6);
+    // document.getElementById("roll6").innerHTML=numberMinMax(1,7)
 }
 
-function dice() {
-    console.log("button clicked")
-    console.log("this is Dice2 dice")
-    numberMINMax(1, 6)
-
-}
+// function Dice10() {
+//     console.log("button clicked")
+//     console.log("This is the Dice10 dice")
+//     numberMinMax(1, 10)
+    // document.getElementById("roll6").innerHTML=numberMinMax(1,7)
+// }
 
 function numberMinMax(min, max) {
-    const diceRoll = Math.floor(Math.random() = (min + 1)) + min;
+    const diceRoll = Math.floor(Math.random() * (max - min + 1)) + min;
     console.log(diceRoll);
-    return diceRoll;
+    return (diceRoll);
 }
